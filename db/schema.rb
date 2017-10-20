3bf1f02df1cce9b87595ad473868beda4d01b3d7
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-20 01:14:19 -0400

    rolled back the last change

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020050812) do
+ActiveRecord::Schema.define(version: 20171020050049) do
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
-    t.integer "quantity"
   end
 
 end
