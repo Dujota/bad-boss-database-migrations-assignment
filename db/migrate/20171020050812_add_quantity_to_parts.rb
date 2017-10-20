620a5f2f059eaab3d0e7dde79c21ff4a57085ca0
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-20 01:13:47 -0400

    added quantity to parts

--- db/migrate/20171020050812_add_quantity_to_parts.rb
+++ db/migrate/20171020050812_add_quantity_to_parts.rb
@@ -1,6 +1,6 @@
 class AddQuantityToParts < ActiveRecord::Migration[5.0]
   def change
-    change_table :parts do |variable|
+    change_table :parts do |t|
       t.integer :quantity
     end
   end
