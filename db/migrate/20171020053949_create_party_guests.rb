7ea84b5a0ec51cbf4dc0d66eb47ec2cd22adf9a8
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-20 01:43:20 -0400

    had some issues with the migrate, fixed, forgot a colon

--- db/migrate/20171020053949_create_party_guests.rb
+++ db/migrate/20171020053949_create_party_guests.rb
@@ -2,7 +2,8 @@ class CreatePartyGuests < ActiveRecord::Migration[5.0]
   def change
     create_table :party_guests do |t|
       t.string :first_name
-      t.stringdietary_restrictions :last_name
+      t.string :last_name
+      t.string :dietary_restrictions
       t.integer :salary
       t.integer :number_of_kids
       t.string :vulnerabilities
